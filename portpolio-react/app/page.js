9e216cf836b1f421ac804cefa1f8f52892279229
권준혁 / Kwon Joonhyeok <45958283+jundoopop@users.noreply.github.com>
2024-06-15 16:07:40 +0900

    feat: component-identity
    # Feature
    
    - It shows identification of the user's profile.
    - Photo, contacts and position would be showed
    
    # Current
    
    - Layout of component is implemented
    - Adding icons and appearance enhancement is in the development.

---
+++ portpolio-react/app/page.js
@@ -0,0 +1,10 @@
+import Identity from "@/components/identity";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Identity />
+    </main>
+  );
+}
---
+++ portpolio-react/components/identity.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Image from "next/image";
+
+const Identity = () => {
+  return (
+    <>
+      <section className="place-content-center">
+        <div className="justify-center place-content-center justify-center border-stone-200 rounded-lg border-solid">
+          <Image
+            src="https://www.svgrepo.com/show/503767/butterfly.svg"
+            alt="butterfly"
+            width={180}
+            height={180}
+          />
+        </div>
+
+        <article className="bg-gray-200 rounded-lg p-4 w-56 max-h-screen">
+          <h2 className="text-4xl">Name</h2>
+          <p className="">Position</p>
+          <ul className="flex justify-around">
+            {/* Github Icon*/}
+            <li className="border-solid w-8 border border-black">
+              <Image
+                src="https://www.svgrepo.com/show/507321/github.svg"
+                alt="GitHub"
+                width={100}
+                height={100}
+              />
+            </li>
+            {/* Instagram Icon*/}
+            <li className="border-solid w-8 border border-black">
+              <Image
+                src="https://www.svgrepo.com/show/452229/instagram-1.svg"
+                alt="Instagram"
+                width={100}
+                height={100}
+              />
+            </li>
+            {/* LinkedIn Icon*/}
+            <li className="border-solid w-8 border border-black">
+              <Image
+                src="https://www.svgrepo.com/show/452051/linkedin.svg"
+                alt="LinkedIn"
+                width={100}
+                height={100}
+              />
+            </li>
+          </ul>
+          <ul className="mt-4 bg-red-300 rounded-lg">
+            <li>Phone</li>
+            <li>Email</li>
+            <li>Location</li>
+            <li>Birthday</li>
+          </ul>
+          <div>
+            <p className="text-sm">Donwload Resume</p>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default Identity;
